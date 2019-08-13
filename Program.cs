8c2d923b1c3ce67a232817d789ccb2c34e000724
smhailey <smhailey@msn.com>
2019-08-13 09:04:59 -0600

    Setup complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day_47_dictionary_of_users
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to the User dictionary");
+    }
+  }
+}
